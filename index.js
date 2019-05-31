687012118702160cbb408cf4614d6e5acac70088
Fernando Mendes <fernando@mendes.codes>
2019-05-31 02:55:48 +0100

    [skip ci] wip

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-var notification = document.getElementsByClassName("Header-link notification-indicator")[0];
-if (notification) notification.parentElement.remove();
+function removeFirstWithClass(className) {
+  var target = document.getElementsByClassName(className)[0];
+
+  if (!target) return;
+
+  var parentElement = target.parentElement;
+
+  target.remove();
+
+  if (parentElement.childElementCount === 0) parentElement.remove();
+}
+
+removeFirstWithClass("Header-link notification-indicator");
+removeFirstWithClass("news");
